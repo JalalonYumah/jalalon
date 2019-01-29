cc75ccc551eaa5760b7e417da9fe007ebb7bba72
JalalonYumah <45378548+JalalonYumah@users.noreply.github.com>
2019-01-29 13:44:51 +0800

    Add files via upload

---
+++ problemB.js
@@ -0,0 +1,15 @@
+function JSN(){
+	let x= XMLHTTPRequest();
+	let data = getElementsById('values');
+
+	xhttp.onreadystate=function{
+		if(xhttp.onreadystate=4 && xhttpstatus = 200)
+
+			let data = JSON.parse(xhttp.response);
+			for (let i=0, i<data.length,i++);
+
+
+
+	}
+xhttp.open("Get","https://jsonplaceholder.typicode")
+}
